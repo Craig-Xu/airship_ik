c58e0fda980d344d0b56d2c40a26e287c839fe44
Craig Xu <jikai.xu@hotmail.com>
2024-12-20 15:52:07 +0800

    更改回调函数为并行

--- src/airship_ik/airship_ik/ik_node.py
+++ src/airship_ik/airship_ik/ik_node.py
@@ -3,8 +3,10 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
 from grasp_interface_pkg.srv import Grasp
+from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
 import numpy as np
 import cv2 as cv
+from time import sleep
 
 def inverse(x, y, z):
     rx = np.sqrt(x**2 + y**2) - 0.095
@@ -46,11 +48,14 @@ class teleop(Node):
         self.target_z = 0
         self.gripper_distance = 0.0
         self.control_pannel = np.zeros((500, 500, 3), np.uint8)
-        self.timer = self.create_timer(0.025, self.timer_callback)
+
+        my_callback_group = ReentrantCallbackGroup()
+
+        self.timer = self.create_timer(0.025, self.timer_callback, callback_group=my_callback_group)
         # self.update_joint = self.create_timer(0.01, self.update_target_joint)
-        self.joint_sub = self.create_subscription(JointState, 'joint_states', self.joint_callback, 10)
+        self.joint_sub = self.create_subscription(JointState, 'joint_states', self.joint_callback, 10,callback_group=my_callback_group)
         self.target_joint = inverse(self.target_x, self.target_y, self.target_z)
-        self.srv = self.create_service(Grasp, 'grasp', self.grasp_callback)
+        self.srv = self.create_service(Grasp, 'grasp', self.grasp_callback, callback_group=my_callback_group)
 
         self.gripper_command = 0
         self.gripper_bool = 0
@@ -79,9 +84,9 @@ class teleop(Node):
         self.target_z = target_z
         
         self.pos_bool = 1
-        self.target_joint = inverse(self.target_x, self.target_y, self.target_z)
 
         if(request.g == 0 or request.g == 1): #遥操作任务
+            self.target_joint = inverse(self.target_x, self.target_y, self.target_z)
             self.gripper_command = request.g
             response.ret = 0
             return response
@@ -89,19 +94,25 @@ class teleop(Node):
         self.gripper_command = 0
         self.gripper_bool = 1
 
-        while self.gripper_bool == 1:
-            print("Gripper Opening")
+        # while self.gripper_bool == 1:
+            # print("Gripper Opening")
+        sleep(1.0) # 等待夹爪打开
         print ("Gripper Opened")
 
+        self.target_joint = inverse(self.target_x, self.target_y, self.target_z)
+
         while self.pos_bool == 1:
+            print(self.now_joint)
             print("Moving to target position")
         
         print("Arrived at target position")
 
+
         self.gripper_command = 1
-        self.gripper_bool = 0
-        while self.gripper_bool == 0:
-            print("Grasping")
+        # while self.gripper_bool == 0:
+        #     print("Grasping")
+        # sleep for 0.5s
+        sleep(1.0)
         print("Grasper Closed")
 
         response.ret = 0
@@ -109,14 +120,14 @@ class teleop(Node):
 
     def joint_callback(self, msg):
         self.now_joint = msg.position[6:12]
-        error = np.linalg.norm(np.array(self.now_joint) - np.array(self.target_joint))
+        error = np.linalg.norm(np.array(self.now_joint) - np.array(self.target_joint[0:6]))
         if error < 0.05:
             self.pos_bool = 0
         else:
             self.pos_bool = 1
         
         gripper_target = 0.0 if self.gripper_command == 0 else -1.0
-        gripper_error = np.abs(msg.position[6] - gripper_target)
+        gripper_error = np.abs(msg.position[15] - gripper_target)
 
         if gripper_error < 0.1: #到达之后切换夹爪状态
             self.gripper_bool = self.gripper_command
@@ -162,6 +173,10 @@ class teleop(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = teleop()
-    rclpy.spin(node)
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(node)
+    executor.spin()
+    executor.shutdown()
     rclpy.shutdown()
     cv.destroyAllWindows()
+
